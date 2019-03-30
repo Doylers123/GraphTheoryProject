6f083b025bda742810e1d0a127da537b9610585b
Doylers123 <34452738+Doylers123@users.noreply.github.com>
2019-03-30 12:51:27 +0000

    Update thompson.py

--- thompson.py
+++ thompson.py
@@ -5,8 +5,8 @@
 # Use None for a label representing "e" arrows
 class state:
     label = None
-    edge1 = None
-    edge2 = None
+    e_arrow1 = None
+    e_arrow2 = None
 
 # An NFA is represented by its initial and accept states
 class nfa:
@@ -22,44 +22,42 @@ def compile(pofix):
 
     for c in pofix:
         if c == '.':
-            # Pop two nfas off the stack
+            # Once the "."" operator is read, Pop two nfas off the stack
             nfa2 = nfastack.pop()
             nfa1 = nfastack.pop()
             # Connct first NFA's accept state to the second's initial.
-            nfa1.accept.edge1 = nfa2.initial
-            # Push NFA to the stack.
+            nfa1.accept.e_arrow1 = nfa2.initial
+            # Push NFA back onto the stack.
             newnfa = nfa(nfa1.initial, nfa2.accept) # using the constructor
-            nfastack.append(newnfa)
+            nfastack.append(newnfa) #new nfa created 
         elif c == '|':
-            #pop two nfas off the stack
+            # Once the "|" operator is read, pop two nfas off the stack
             nfa2 = nfastack.pop()
             nfa1 = nfastack.pop()
-            # Create a new initial state, connect it to initial states
-            # of the two NFA's popped from the stack.
+            # Create a new initial state, connect it to initial states of the two NFA's popped from the stack.
             initial = state()
-            initial.edge1 = nfa1.initial
-            initial.edge2 = nfa2.initial
-            #Create a new accept state, connecting the accept states
-            # of the two NFA's popped from the stack, to the new state.
+            initial.e_arrow1 = nfa1.initial
+            initial.e_arrow2 = nfa2.initial
+            #Create a new accept state, connecting the accept states of the two NFA's popped from the stack, to the new state.
             accept = state()
-            nfa1.accept.edge1 = accept
-            nfa2.accept.edge1 = accept
+            nfa1.accept.e_arrow1 = accept
+            nfa2.accept.e_arrow1 = accept
             #Push new NFA to the stack
             newnfa = nfa(initial, accept)
             nfastack.append(newnfa)
         elif c == '*':
-            #
+            # Pop nfa off the stack
             nfa1 = nfastack.pop()
-            #
+            # Create a new initial state and a new accept state
             initial = state()
             accept = state ()
-            #
-            initial.edge1 = nfa1.initial
-            initial.edge2 = accept
-            #
-            nfa1.accept.edge1 = nfa1.initial
-            nfa1.accept.edge2 = accept
-            #
+            # Joins your newly created initial state to your nfa1's initial state and your accept state
+            initial.e_arrow1 = nfa1.initial
+            initial.e_arrow2 = accept
+            # Joins your old accept state to the new accept state and to nfa1's initial state. 
+            nfa1.accept.e_arrow1 = nfa1.initial
+            nfa1.accept.e_arrow2 = accept
+            # Put your new nfa back onto the stack.
             newnfa = nfa(initial, accept)
             nfastack.append(newnfa)
         else:
@@ -68,7 +66,7 @@ def compile(pofix):
             initial = state()
             # Join the initial state to the accept state using an arrow labelled c.
             initial.label = c
-            initial.edge1 = accept
+            initial.e_arrow1 = accept
             # Push new NFA to the stack.
             newnfa = nfa(initial, accept)
             nfastack.append(newnfa)
@@ -78,3 +76,5 @@ def compile(pofix):
 
 print(compile("ab.cd.|"))
 print(compile("aa.*"))
+print(compile("ac.b*"))
+print(compile("ac.b|"))
